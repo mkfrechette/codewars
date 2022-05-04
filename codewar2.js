55b4ad32980c7deede792cfa6f72669261886d33
MK Frechette <mollykatefrechette@gmail.com>
2022-05-03 21:48:49 -0400

    add daily coding challenge

---
+++ codewar2.js
@@ -0,0 +1,4 @@
+//Create a function that takes in an array of numbers. Return a new array of numbers that is every original number squared
+
+let nums = [40,20,70,30,50]
+let numsSquared = nums.map(n => n * n)
