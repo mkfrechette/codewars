c8c6608e000b8c7f90337aa2aafaa45bb69cc6d1
MK Frechette <mollykatefrechette@gmail.com>
2022-05-13 16:34:45 -0400

    adding daily codewars challenge

---
+++ solution016.js
@@ -0,0 +1,10 @@
+//Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+function between(a, b) {
+    // your code here
+    arr = []
+    for(i = a;i <= b; i++){ 
+        arr.push(i)
+    }
+    return arr
+  }
