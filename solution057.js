4e1a03cc56103d5ebefd9a6fd3e88d445541b86b
MK Frechette <mollykatefrechette@gmail.com>
2022-08-10 13:25:23 -0400

    adding daily coding challenge and PREP

---
+++ solution057.js
@@ -0,0 +1,19 @@
+/*Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.*/
+
+
+
+//make the function an arrow function
+//add the 3 goal values together with implicit return due to arrow function
+let goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals
+//P: 3 different number values, always whole numbers and positive
+//R: the sum of the 3 numbers
+//E: 5, 5, 5 -> 15
+//10 15 5 -> 30
+//2 8 10 -> 20
