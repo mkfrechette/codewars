f1217cae5689191fee018228d593a47eb238f24e
MK Frechette <mollykatefrechette@gmail.com>
2022-10-02 11:52:02 -0400

    changed solution to an arrow function

--- solution072.js
+++ solution072.js
@@ -1,11 +1,12 @@
 //Given a sequence of numbers, find the largest pair sum in the sequence.
 
-function largestPairSum (numbers) {
+
     //loop through and sort array in ascending order
     //target the last two integers using slice
     //add nums together using reduce, return
-    return numbers.sort((a,b) => a - b).slice(-2).reduce((acc,c) => acc + c, 0)
-  }
+
+    let largestPairSum = numbers => numbers.sort((a,b) => a - b).slice(-2).reduce((acc,c) => acc + c, 0)  
+    
     //P: an array of numbers, whole numbers, maybe negative, minimum 2 integers in arr
     //R: the sum of the two largest integers in the array
     //E: [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
